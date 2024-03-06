2025475d848a49135e43d2d8ff1b451bf8461234
supriyagit24 <154308501+supriyagit24@users.noreply.github.com>
2024-03-06 15:48:33 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull supriyajadhav24/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 supriyajadhav24/simple-python-flask-app
